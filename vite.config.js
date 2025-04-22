0c002ff6f1604a1659521f4d6ab2e04e9c776d46
Cömert Baldemir <comertbaldemir@gmail.com>
2025-04-22 11:24:11 +0300

    Build fail problem fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+import { webcrypto } from 'crypto';
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto;
+}
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+});
